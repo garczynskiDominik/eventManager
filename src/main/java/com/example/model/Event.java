182041eaba7ab07401ca267d35b23bce8d20a8b2
garczynskiDominik <garczynski.dominik@gmail.com>
2021-06-26 14:28:58 +0200

    added dependency to h2 and configured conection to database

---
+++ src/main/java/com/example/model/Event.java
@@ -0,0 +1,30 @@
+package com.example.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class Event{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NonNull
+    private String nameOfEvent;
+    private Date startDate;
+    private Date endDate;
+    private String description;
+
+}
