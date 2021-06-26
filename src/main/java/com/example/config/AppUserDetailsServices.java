c306152154cf25571afd7c11d39c4c2a4cc04c61
garczynskiDominik <garczynski.dominik@gmail.com>
2021-06-26 15:43:50 +0200

    new pom and Readme

--- src/main/java/com/example/config/AppUserDetailsServices.java
+++ src/main/java/com/example/config/AppUserDetailsServices.java
@@ -1,6 +1,7 @@
 package com.example.config;
 
 
+import com.example.model.UserEntityDetails;
 import com.example.repository.UserEntityRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,6 +16,9 @@ public class AppUserDetailsServices implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return  userEntityRepository
+                .findByUsernameIgnoreCase(username)
+                .map(UserEntityDetails::new)
+                .orElseThrow(()-> new UsernameNotFoundException(username));
     }
 }
--- src/main/java/com/example/repository/UserEntityRepository.java
+++ src/main/java/com/example/repository/UserEntityRepository.java
@@ -2,10 +2,11 @@ package com.example.repository;
 
 import com.example.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
 
-    Optional<UserEntity> findByUserNameIgnoreCase();
+    Optional<UserEntity> findByUsernameIgnoreCase(String userName);
 }
